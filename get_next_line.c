1943cfd7df329be08813fa781c0aeb1b117df274
Shinowtf <Shinowtf@Shinowtfs-MacBook-Pro.local>
2024-04-08 12:42:58 +0800

    mandatory done

--- get_next_line.c
+++ get_next_line.c
@@ -6,47 +6,47 @@
 /*   By: lkah-chu <lkah-chu@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 20:35:18 by lkah-chu          #+#    #+#             */
-/*   Updated: 2024/04/07 17:28:04 by lkah-chu         ###   ########.fr       */
+/*   Updated: 2024/04/08 12:29:47 by lkah-chun        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char    *read_line_to_leftover(int fd, char *leftover_string)
+char	*read_line_to_leftover(int fd, char *leftover_string)
 {
-    char   *buffer;
-    int    bytes_read;
+	char	*buffer;
+	int		bytes_read;
 
-    buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    if(!buffer)
-        return (NULL);
-    bytes_read = 1;
-    while(!ft_strchr(leftover_string, '\n') && bytes_read != 0)
-    {
-        bytes_read = read(fd, buffer, BUFFER_SIZE);
-        if(bytes_read == -1)
-        {
-            free(buffer);
-            return (NULL);
-        }
-        buffer[bytes_read] = '\0';
-        leftover_string = ft_strjoin(leftover_string, buffer);
-    }
-    free(buffer);
-    return (leftover_string);
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	bytes_read = 1;
+	while (!ft_strchr(leftover_string, '\n') && bytes_read != 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[bytes_read] = '\0';
+		leftover_string = ft_strjoin(leftover_string, buffer);
+	}
+	free(buffer);
+	return (leftover_string);
 }
 
-char    *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    char    *one_line;
-    static char    *static_leftover_string;
+	char		*one_line;
+	static char	*static_leftover_string;
 
-    if(fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    static_leftover_string = read_line_to_leftover(fd, static_leftover_string);
-    if(!static_leftover_string)
-        return (NULL);
-    one_line = get_one_line(static_leftover_string);
-    static_leftover_string = get_leftover_string(static_leftover_string);
-    return (one_line);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	static_leftover_string = read_line_to_leftover(fd, static_leftover_string);
+	if (!static_leftover_string)
+		return (NULL);
+	one_line = get_one_line(static_leftover_string);
+	static_leftover_string = get_leftover_string(static_leftover_string);
+	return (one_line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,40 +6,120 @@
 /*   By: lkah-chu <lkah-chu@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 16:57:49 by lkah-chu          #+#    #+#             */
-/*   Updated: 2024/04/07 17:50:14 by lkah-chu         ###   ########.fr       */
+/*   Updated: 2024/04/08 12:39:55 by lkah-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t  ft_strlen(char *str)
+size_t	ft_strlen(char *str)
 {
-    size_t  i;
+	size_t	i;
 
-    i = 0;
-    if(!str)
-        return (0);
-    while(str[i])
-        i++;
-    return (i);
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
 }
 
-char    *ft_strchr(char *s, int c)
+char	*ft_strchr(char *s, int c)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if(!s)
-        return (NULL);
-    if(c == '\0')
-        return (s + ft_strlen(s));
-    while(s[i])
-    {
-        if(s[i] == c)
-            return (s + i);
-        i++;
-    }
-    return(0);
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (c == '\0')
+		return (s + ft_strlen(s));
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	return (0);
 }
 
-char    *ft_strjoin()
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	if (!s1)
+	{
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	j = 0;
+	if (s1)
+		while (s1[++i] != '\0')
+			str[i] = s1[i];
+	while (s2[j] != '\0')
+		str[i++] = s2[j++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (str);
+}
+
+char	*get_one_line(char *left_str)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!left_str[i])
+		return (NULL);
+	while (left_str[i] && left_str[i] != '\n')
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 2));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (left_str[i] && left_str[i] != '\n')
+	{
+		str[i] = left_str[i];
+		i++;
+	}
+	if (left_str[i] == '\n')
+	{
+		str[i] = left_str[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*get_leftover_string(char *left_str)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (left_str[i] && left_str[i] != '\n')
+		i++;
+	if (!left_str[i])
+	{
+		free(left_str);
+		return (NULL);
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(left_str) - i + 1));
+	if (!str)
+		return (NULL);
+	i++;
+	j = 0;
+	while (left_str[i])
+		str[j++] = left_str[i++];
+	str[j] = '\0';
+	free(left_str);
+	return (str);
+}
